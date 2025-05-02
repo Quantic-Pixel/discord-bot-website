109b569c314729ea2ffbe55c32c013c6b4d84548
M4rshe1 <colin.heggli@gmail.com>
2025-05-02 14:06:24 +0200

    Rename section header from German to English

--- components/features.tsx
+++ components/features.tsx
@@ -3,7 +3,7 @@ import {FEATURES} from "@/settings/features";
 const Features = () => {
     return  <div className="mt-20">
         <h2 className="text-3xl font-bold text-white text-center mb-12">
-            Was kannst du machen?
+            Features
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {
